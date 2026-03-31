5eec2e7e9e42c21024472e4af0a03ab074899170
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-31 17:01:39 +0800

    feat:authentication

--- app/Models/TemplateModel.php
+++ app/Models/TemplateModel.php
@@ -2,19 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['name_template', 'slug_template', 'thumbnail', 'is_active'])]
 class TemplateModel extends Model
 {
     use HasFactory, HasUlids;
-
     protected $table = 'tb_template';
-    protected $fillable = [
-        'name_template',
-        'slug_template',
-        'thumbnail',
-        'is_active',
-    ];
 }
