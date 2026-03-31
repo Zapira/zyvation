bad2d0ccbfe898b0c32302196599b4369bd7f00d
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-31 17:02:05 +0800

    feat:authentication

---
+++ resources/js/pages/auth/register.jsx
@@ -0,0 +1,131 @@
+import { GrTemplate } from "react-icons/gr";
+import { Link } from "react-router-dom";
+import { RegisterHook } from "../../hooks/auth/registerHook";
+
+export default function Register() {
+    const { register, handleSubmit, errors, loading, onSubmit, onError } = RegisterHook();
+    return (
+        <div className="min-h-screen flex items-center justify-center ">
+            <div className="w-full p-2 max-w-4xl mx-auto">
+                <div className="flex rounded-2xl overflow-hidden shadow-2xl">
+
+                    <div className="hidden md:flex flex-col justify-between flex-1 p-14 relative overflow-hidden
+                     bg-[#4F46E5]">
+                        <div className="absolute -top-20 -right-20 w-72 h-72 
+    bg-white opacity-10 rounded-full"></div>
+                        <div className="absolute top-80 -left-20 w-72 h-72 
+    bg-white opacity-10 rounded-full "></div>
+                        <div className="absolute inset-0 opacity-[0.03] bg-[url('data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.03'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+
+                        <div className="flex items-center space-x-3 relative z-10">
+                            <div className="w-8 h-8 rounded-lg bg-white/15  text-white flex items-center justify-center text-sm">
+                                ZY
+                            </div>
+                            <div className="text-white font-bold tracking-[3px] text-lg">
+                                Zyvation
+                            </div>
+                        </div>
+
+                        <div className="mt-10 relative z-10">
+                            <div className="flex items-center rounded-2xl bg-white/20 p-2 text-sm w-2/3 backdrop-blur-md">
+                                <div className="w-1 h-1 bg-green-400 rounded-full inline-block mr-2"></div>
+                                <span className="text-white">Gratis untuk memulai</span>
+                            </div>
+                            <h1 className="text-white font-bold text-3xl leading-snug mt-4">
+                                Buat undangan impianmu sekarang!
+                            </h1>
+
+                            <p className="text-white/70 mt-4 max-w-md text-sm">
+                                Bergabung dengan puluhan ribu pasangan yang telah mempercayakan momen spesial mereka kepada Zyvation.
+                            </p>
+                        </div>
+
+                        <div className="space-y-3 pt-10 relative z-10 text-sm">
+
+                            <div className="flex items-center text-white font-semibold">
+                                <div className="w-8 h-8 bg-white/15 rounded-lg flex items-center justify-center"><GrTemplate /></div>
+                                <span className="ml-3">Berbagai macam template gratis</span>
+                            </div>
+
+                            <div className="flex items-center text-white font-semibold">
+                                <div className="w-8 h-8 bg-white/15 rounded-lg flex items-center justify-center">📊</div>
+                                <span className="ml-3">Analisis dan laporan yang lengkap</span>
+                            </div>
+
+                            <div className="flex items-center text-white font-semibold">
+                                <div className="w-8 h-8 bg-white/15 rounded-lg flex items-center justify-center">⚙️</div>
+                                <span className="ml-3">Kemudahan dalam penggunaan</span>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="w-full md:w-96 bg-white flex flex-col justify-center items-center mb-10">
+                        <h3 className="text-2xl font-bold text-[#0b1f3a] mt-10">
+                            Buat Akun Baru
+                        </h3>
+
+                        <p className="text-gray-500 text-sm mb-6">
+                            Sudah punya akun? <Link to="/login" className="text-[#4F46E5] hover:underline">Masuk di sini</Link>
+                        </p>
+
+                        <form className="p-8 py-3" onSubmit={handleSubmit(onSubmit, onError)}>
+                            <div className="flex gap-4 md:flex-row flex-col w-full">
+                                <div className="mt-4">
+                                    <label className="text-xs font-bold text-gray-700">Nama Depan</label>
+                                    <input type="text"
+                                        {...register('first_name', { required: 'Nama depan wajib diisi' })}
+                                        className={`w-full mt-1 px-4 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${errors.first_name ? 'border-red-500' : ''}`}
+                                        placeholder="Nama Depan" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="text-xs font-bold text-gray-700">Nama Belakang</label>
+                                    <input type="text"
+                                        {...register('last_name')}
+                                        className={`w-full mt-1 px-4 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${errors.last_name ? 'border-red-500' : ''}`}
+                                        placeholder="Nama Belakang" />
+                                </div>
+                            </div>
+
+                            <div className="mt-4">
+                                <label className="text-xs font-bold text-gray-700">Alamat Email</label>
+                                <input type="email"
+                                    {...register('email', { required: 'Alamat email wajib diisi' })}
+                                    className={`w-full mt-1 px-4 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${errors.email ? 'border-red-500' : ''}`}
+                                    placeholder="nama@email.com" />
+                            </div>
+                            <div className="mt-4">
+                                <label className="text-xs font-bold text-gray-700">Kata Sandi</label>
+                                <input type="password"
+                                    {...register('password', { required: 'Kata sandi wajib diisi' })}
+                                    className={`w-full mt-1 px-4 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${errors.password ? 'border-red-500' : ''}`}
+                                    placeholder="••••••••" />
+                            </div>
+
+                            <div className="mt-4">
+                                <label className="text-xs font-bold text-gray-700">Konfirmasi Kata Sandi</label>
+                                <input type="password"
+                                    {...register('password_confirmation', { required: 'Konfirmasi kata sandi wajib diisi' })}    
+                                    className={`w-full mt-1 px-4 py-2 text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${errors.password_confirmation ? 'border-red-500' : ''}`}
+                                    placeholder="••••••••" />
+                            </div>
+
+                            <button type="submit"
+                                className="w-full mt-4 py-3 rounded-lg font-bold text-sm text-white 
+                            bg-linear-to-r from-blue-500 to-blue-700 cursor-pointer
+                            shadow-lg shadow-blue-500/30 hover:opacity-90 transition">
+                                {loading ? 'Mendaftar...' : 'Daftar Sekarang'}
+                            </button>
+
+                            <p className="text-center text-xs text-gray-400 mt-4">
+                                Dengan masuk, Anda menyetujui <a href="#" className="text-[#4F46E5] hover:underline">Syarat & Ketentuan</a> kami.
+                            </p>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
