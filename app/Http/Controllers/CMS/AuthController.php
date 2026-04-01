0ebd625cfdd333cfad285ff393baeac60cf8b07e
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-01 18:45:09 +0800

    feat:authentication

--- app/Http/Controllers/CMS/AuthController.php
+++ app/Http/Controllers/CMS/AuthController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Interfaces\AuthInterfaces;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
