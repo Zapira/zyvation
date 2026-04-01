633ece7cab0de847913df3db53be1e6c0b3943bd
Rizki <muhammadrizkitkj123@gmail.com>
2026-04-01 18:46:05 +0800

    feat:authentication

--- resources/js/api/interceptor.js
+++ resources/js/api/interceptor.js
@@ -1,17 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_URL, withCredentials: true
-})
+    baseURL: import.meta.env.VITE_APP_URL,
+    withCredentials: true
+});
 
 api.interceptors.request.use(
-    response=> response,
+    config => config,
+    error => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+    response => response,
     error => {
-        if (error.response && error.response.status === 401 && window.location.pathname !== "/login") {
+        if (
+            error.response &&
+            error.response.status === 401 &&
+            window.location.pathname !== "/login"
+        ) {
             window.location.href = "/login";
         }
         return Promise.reject(error);
     }
-)
+);
 
 export default api;
